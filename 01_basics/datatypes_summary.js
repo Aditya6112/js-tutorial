d42973560ea8f7b898fcc0d4de581aca4f765cea
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2024-01-04 07:03:07 +0000

    Stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -28,10 +28,36 @@ let myObj = {
     age: 20,
 }
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello world");
 }
 
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack => Primitive, Heap=> Non-Primitive
+
+let myhomeName = "aditya"
+let anothername = myhomeName
+anothername = "adi"
+
+console.log(myhomeName);   //aditya
+console.log(anothername);  //adi
+//not changed because it makes a copy not changed original value.
+
+
+//now in heap it uses reference
+let userOne = {
+    email: "user1@gmail.com",
+    upi: "user1@ybl",
+}
+
+let userTwo = userOne //due to heap it changed its original value.
+
+userTwo.email = "changedEmail@gmail.com"
+
+console.log(userOne);    //"changedEmail@gmail.com"
+console.log(userTwo);    //"changedEmail@gmail.com"
